73e89b1b279cdf3e8724a4873104976ab1bf102a
Peter Eliasson <warackta@gmail.com>
2015-02-05 19:24:27 +0100

    Updated record defs.
    Added to client:
    * server the client is connected to
    * current nick of the client.
    
    Server:
    * clients - A dict of currently connected clients.

--- defs.hrl
+++ defs.hrl
@@ -1,9 +1,20 @@
 % This record defines the structure of the client process.
 % It contains the following fields:
 %   gui: the name (or Pid) of the GUI process.
--record(cl_st, {gui}).
+%   server: the name (or Pid) of the server process.
+%   nick: The nick of the client.
+-record(cl_st, {
+  gui,
+  server,
+  nick
+}).
 
 % This record defines the structure of the server process.
 % It contains the following fields:
+%   server: the name of the server process.
 %
--record(server_st, {}).
+%
+-record(server_st, {
+  server,
+  clients
+}).
