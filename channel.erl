2a15eafe2d90410950306b35d7c35ce58dc5cb71
Peter Eliasson <warackta@gmail.com>
2015-02-06 20:20:06 +0100

    Finished channel functionallity.
    * Channel now holds a list of Client PIDs that is connected, so it knows where to send messages.
    * The add/remove_client methods are used to add/remove a client from the list of PIDs.
    * Sending messages is done by spawning a new process, to avoid blocking or something. This should be benchmarked further to make sure it is the best way.

--- channel.erl
+++ channel.erl
@@ -2,10 +2,26 @@
 -include_lib("./defs.hrl").
 
 %% API
--export([init/1, start/1]).
+-export([init/2, start/2, add_client/2, remove_client/2]).
 
-loop(St) ->
+loop(St = #channel_st{clients = Clients, name = ChannelName}) ->
   receive
+    {request, From, Ref, {add_client, ClientPid}} ->
+      NewClients = [ClientPid | Clients],
+      From ! {result, Ref, ok},
+      loop(St#channel_st{clients = NewClients});
+    {request, From, Ref, {remove_client, ClientPid}} ->
+      NewClients = lists:delete(ClientPid, Clients),
+      From ! {result, Ref, ok},
+      loop(St#channel_st{clients = NewClients});
+    {request, From, _Ref, {message, Nick, Message}} ->
+      % TODO: benchmark this further, for perf tests.
+      spawn(fun() ->
+        lists:foreach(fun(ClientPid) ->
+          ClientPid ! {message, ChannelName, Nick, Message}
+        end, lists:delete(From, Clients))
+      end),
+      loop(St);
     Msg ->
       % To make sure nothing is sending accidental messages to
       % our channel; catch all clause.
@@ -13,8 +29,14 @@ loop(St) ->
       loop(St)
   end.
 
-init(ChannelName) ->
-  loop(ChannelName).
+init(ChannelName, Clients) ->
+  loop(#channel_st{name = ChannelName, clients = Clients}).
 
-start(ChannelName) ->
-  spawn(?MODULE, init, [ChannelName]).
+start(ChannelName, Clients) ->
+  spawn(?MODULE, init, [ChannelName, Clients]).
+
+add_client(ChannelPid, ClientPid) ->
+  helper:request(ChannelPid, {add_client, ClientPid}).
+
+remove_client(ChannelPid, ClientPid) ->
+  helper:request(ChannelPid, {remove_client, ClientPid}).
